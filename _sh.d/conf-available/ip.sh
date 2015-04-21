547a91418c1d711520f258fb16e6a3567da25c94
Fabio Tea <ft2011@gmail.com>
2015-04-21 09:01:54 +0200

    save

--- _sh.d/conf-available/ip.sh
+++ _sh.d/conf-available/ip.sh
@@ -1,11 +1,11 @@
 #######
 ## 
 #
-publicip() {
-	ipechoService=("http://ipecho.fftt.info/plain.php" "http://ifconfig.me/ip")
+#publicip() {
+	ipechoService=("http://ipecho.ft11.de/?raw" "http://ifconfig.me/ip")
 	for service in $ipechoService; do
 		curl --max-time 10 $service
 		if [[ $? == 0 ]]; then break; fi
 	done
-}
+#}
 
--- _sh.d/conf-available/locales.sh
+++ _sh.d/conf-available/locales.sh
@@ -3,4 +3,6 @@
 #
 export LANG=en_US.UTF-8
 export LC_MESSAGES="C"
-export LC_ALL=en_US.UTF-8
+export LC_DATE=de_DE.utf8
+export LC_NUMERIC=de_DE.utf8
+export LC_TIME=de_DE.utf8
