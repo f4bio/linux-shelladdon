132141f43524fa4a7a49899fc7bb311fdf71e542
Fabio Tea <ft2011@gmail.com>
2015-04-21 09:02:23 +0200

    save

--- _sh.d/conf-available/ip.sh
+++ _sh.d/conf-available/ip.sh
@@ -1,11 +1,11 @@
 #######
 ## 
 #
-#publicip() {
+publicip() {
 	ipechoService=("http://ipecho.ft11.de/?raw" "http://ifconfig.me/ip")
 	for service in $ipechoService; do
 		curl --max-time 10 $service
 		if [[ $? == 0 ]]; then break; fi
 	done
-#}
+}
 
